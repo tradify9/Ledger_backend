2ffcb25259f12e5c8c7bdfd8e1c6242a015c0337
Ganesh Prajapati <ganeshrprajapati29@gmail.com>
2026-05-04 22:16:31 +0530

    latest push

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,15 +1,23 @@
-// routes/contactDetailsRoutes.js
 import express from 'express';
 import { protect, admin } from '../middleware/auth.js';
-import { getContactDetails, updateContactDetails } from '../controllers/contactDetailsController.js';
+import { 
+  submitContact, 
+  getContacts, 
+  updateContactReply, 
+  deleteContact,
+  updateContact 
+} from '../controllers/contactsController.js';
 
 const router = express.Router();
 
-// Public route - anyone can view
-router.get('/', getContactDetails);
+// Public route - submit contact form
+router.post('/', submitContact);
 
 // Protected admin routes
 router.use(protect, admin);
-router.put('/', updateContactDetails);
+router.get('/', getContacts);
+router.put('/:id', updateContact);
+router.put('/:id/reply', updateContactReply);
+router.delete('/:id', deleteContact);
 
 export default router;
